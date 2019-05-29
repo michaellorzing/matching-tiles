1d36d7dbd2894043c96b11e6d247101ff805e15c
michael Lorzing <ghostsofgrace@gmail.com>
2019-05-29 12:13:30 -0400

    Updates

---
+++ precache-manifest.06b426e97f42297aa37669cd0a586809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b709ddfaaa12b564445dd85942ac0a",
+    "url": "/matching-tiles/index.html"
+  },
+  {
+    "revision": "7d5f3af75b1f83fb7315",
+    "url": "/matching-tiles/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "03c98dc8f5f6037c0d58",
+    "url": "/matching-tiles/static/js/2.3d0c7bc5.chunk.js"
+  },
+  {
+    "revision": "7d5f3af75b1f83fb7315",
+    "url": "/matching-tiles/static/js/main.04d9f050.chunk.js"
+  },
+  {
+    "revision": "eb8f7d12a33001b5c550",
+    "url": "/matching-tiles/static/js/runtime~main.a954ece8.js"
+  }
+]);
